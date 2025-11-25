9c98b521c77049e80c91fdff57c98848204377b1
Kyxie <kyriexie@outlook.com>
2025-11-24 20:20:07 -0500

    update js

--- routes/script.go
+++ routes/script.go
@@ -6,33 +6,35 @@ import (
 
 func RegisterScriptRoutes(r *gin.Engine) {
 	r.GET("/script.js", func(c *gin.Context) {
-
 		script := `(function () {
-	const currentScript = document.currentScript;
-	const initVisitors = parseInt(currentScript.getAttribute('init-visitors') || '0', 10);
-	const initViews = parseInt(currentScript.getAttribute('init-views') || '0', 10);
-	const origin = currentScript.src.split('/script.js')[0];
+    const currentScript = document.currentScript;
+    const initVisitors = parseInt(currentScript.getAttribute('init-visitors') || '0', 10);
+    const initViews = parseInt(currentScript.getAttribute('init-views') || '0', 10);
+    const origin = currentScript.src.split('/script.js')[0];
 
-	const startAt = new Date('2020-01-01').getTime();
-	const endAt = Date.now();
-	const url = origin + '/proxy?startAt=' + startAt + '&endAt=' + endAt;
+    const startAt = new Date('2020-01-01').getTime();
+    const endAt = Date.now();
+    const url = origin + '/proxy?startAt=' + startAt + '&endAt=' + endAt;
 
-	fetch(url)
-		.then(res => res.json())
-		.then(data => {
-		const visitors = (data.visitors?.value || 0) + initVisitors;
-		const views = (data.pageviews?.value || 0) + initViews;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const fetchedVisitors = parseInt(data.visitors || '0', 10); 
+            const fetchedViews = parseInt(data.pageviews || '0', 10); 
 
-		const uvSpan = document.getElementById('uv-count');
-		const pvSpan = document.getElementById('pv-count');
+            const visitors = fetchedVisitors + initVisitors;
+            const views = fetchedViews + initViews;
 
-		if (uvSpan) uvSpan.textContent = visitors.toLocaleString();
-		if (pvSpan) pvSpan.textContent = views.toLocaleString();
-		})
-		.catch(err => {
-		console.error('Umami fetch data error:', err);
-		});
-	})();`
+            const uvSpan = document.getElementById('uv-count');
+            const pvSpan = document.getElementById('pv-count');
+
+            if (uvSpan) uvSpan.textContent = visitors.toLocaleString();
+            if (pvSpan) pvSpan.textContent = views.toLocaleString();
+        })
+        .catch(err => {
+            console.error('Umami fetch data error:', err);
+        });
+})();`
 
 		c.Header("Content-Type", "application/javascript")
 		c.String(200, script)
